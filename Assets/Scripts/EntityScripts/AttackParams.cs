5e3e6dc4069cc0ce1c9c589c8d8f8d96c4254705
iniarski <filipmw2002@gmail.com>
2024-04-23 16:24:48 +0200

    Add damage logic

---
+++ Assets/Scripts/EntityScripts/AttackParams.cs
@@ -0,0 +1,12 @@
+﻿namespace GameNamespace
+{
+    public class AttcakParams
+    {
+        public static readonly byte MELEE = 0;
+        public static readonly byte RANGED = 1;
+        public static readonly byte FIRE = 2;
+        public static readonly byte TOXINE = 3;
+    }
+    
+    public A
+}

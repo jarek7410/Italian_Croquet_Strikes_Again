f66dbdc7260de11089cecbdf4af6d7777964fa2e
jarek <jarekomsta@gmail.com>
2024-05-05 11:51:11 +0200

    more files

---
+++ Assets/Scripts/EnemyScript/RatMovment.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RatMovment : MonoBehaviour
+{
+    private Vector3 target;
+
+    private NavMeshAgent agent;
+    [SerializeField] private GameObject TargetObject;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTargetPosition();
+        SetAgentPosition();
+    }
+
+    void SetTargetPosition()
+    {
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     target = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        // }
+        target = TargetObject.transform.position;
+    }
+
+    void SetAgentPosition()
+    {
+        agent.SetDestination(new Vector3(target.x, target.y,transform.position.z));
+    }
+}

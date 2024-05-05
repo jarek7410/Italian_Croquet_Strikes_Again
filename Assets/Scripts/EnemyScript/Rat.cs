cb3343496317ed8a7197597a543356330fc05627
jarek <jarekomsta@gmail.com>
2024-05-05 11:49:50 +0200

    rat is moving

--- Assets/Scripts/EnemyScript/Rat.cs
+++ Assets/Scripts/EnemyScript/Rat.cs
@@ -17,6 +17,7 @@ namespace EnemyScript
 
         protected override Vector2 GetMovement()
         {
+            return Vector2.zero;
             Vector2 movementVector=Vector2.zero;
             Vector3 distToPlayer = player.transform.position - transform.position;
             Vector2 distToPlayer2D = new Vector2(distToPlayer.x, distToPlayer.y);

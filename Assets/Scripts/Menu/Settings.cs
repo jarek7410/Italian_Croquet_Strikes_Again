f2bdd969ecd931150401416c652950a842b333f4
jarek7410 <39216951+jarek7410@users.noreply.github.com>
2024-06-11 11:55:11 +0200

    start menu added

---
+++ Assets/Scripts/Menu/Settings.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Compilation;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+   public AudioMixer AudioMixer;
+   public TMP_Dropdown resolutionDropdown;
+
+   private Resolution[] _resolutions;
+
+   private void Start()
+   {
+      _resolutions = Screen.resolutions;
+      resolutionDropdown.ClearOptions();
+      List<string> options = new List<string>();
+      int currentRessolutionIndex = 0;
+      for (int i = 0; i < _resolutions.Length; i++)
+      {
+         string opstion = _resolutions[i].width + " x " + _resolutions[i].height;
+         options.Add(opstion);
+
+         if (_resolutions[i].width == Screen.currentResolution.width &&
+             _resolutions[i].height == Screen.currentResolution.height)
+         {
+            currentRessolutionIndex = i;
+         }
+      }
+      resolutionDropdown.AddOptions(options);
+      resolutionDropdown.value = currentRessolutionIndex;
+      resolutionDropdown.RefreshShownValue();
+   }
+
+   public void SetVolume(float volume)
+   {
+      AudioMixer.SetFloat("volume", volume);
+   }
+
+   public void SetFullscreen(bool isFullscreen)
+   {
+      Screen.fullScreen = isFullscreen;
+   }
+
+   public void SetResolution(int resolutionIndex)
+   {
+      Resolution resolution = _resolutions[resolutionIndex];
+      Screen.SetResolution(resolution.width,resolution.height,Screen.fullScreen);
+   }
+}

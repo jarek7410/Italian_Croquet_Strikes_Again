10c92b531d3bfc26f92f2807d224448ebac1cb7c
jarek7410 <39216951+jarek7410@users.noreply.github.com>
2024-06-13 01:10:25 +0200

    prawie działą

--- Assets/Scripts/GameLogic/ChosenSettings.cs
+++ Assets/Scripts/GameLogic/ChosenSettings.cs
@@ -1,7 +1,8 @@
 using GameNamespace;
+using UnityEngine;
 
 public class ChosenSettings {
     // singleton
     public static ChosenSettings Instance {get; private set;} = new ChosenSettings();
-    public BasicRangedWeapon chosenWeapon;
+    public GameObject chosenWeapon;
 }

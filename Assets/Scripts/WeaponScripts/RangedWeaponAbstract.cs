29fd1edad58bc708a95b0159e523f74ed2139af1
iniarski <filipmw2002@gmail.com>
2024-06-12 15:09:32 +0200

    Make weapon params public

--- Assets/Scripts/WeaponScripts/RangedWeaponAbstract.cs
+++ Assets/Scripts/WeaponScripts/RangedWeaponAbstract.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public abstract class RangedWeaponAbstract : WeaponAbstract
 {
     [SerializeField] protected PlayerAbstract player;
-    [SerializeField] protected RangedWeaponParams weaponParams;
+    [SerializeField] public RangedWeaponParams weaponParams { get; protected set; }
     [SerializeField] protected Bullet bullet;
     [SerializeField] private float distanceToPlayer = .7f;
     protected bool isReloading = false;
